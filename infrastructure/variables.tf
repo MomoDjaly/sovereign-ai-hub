81d8c00a3e6e67b0aafec1e1a69d81aedbbb04dc
MomoDjaly <jallil.diab45@gmail.com>
2026-03-10 21:26:54 +0100

    feat: terraform provider aws configured

---
+++ infrastructure/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "La région AWS à utiliser"
+  type        = string
+  default     = "eu-west-3"
+}
+
+variable "project_name" {
+  description = "Le nom du projet"
+  type        = string
+  default     = "sovereign-ai-hub"
+}
